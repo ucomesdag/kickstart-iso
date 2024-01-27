9dd69ceb7eeb488d2a5f643de95e3a7287721dc9
Uco Mesdag <uco@mesd.ag>
2024-01-27 12:29:03 +0100

    feat(debian): Added preseed as an automated install option.

--- kickstart-iso.sh
+++ kickstart-iso.sh
@@ -3,15 +3,17 @@
 
 # Copyright (C) 2014-2023 Uco Mesdag
 # Usage:    kickstart-iso.sh [OPTIONS]
-#   -i netinstall.iso               set the path of the iso file.
-#   -o custom.img                   set the path of the image file or an output
-#                                   directory to create the image file.
+#   -i netinstall.iso               set the path of the ISO file.
+#   -o custom.iso                   set the path of the ISO file or an output
+#                                   directory to create the ISO file.
 #   -k ks.cfg                       set the path of the kickstart configuration
 #                                   file.
+#   -p preseed.cfg                  set the path of the preseed configuration"
+#                                   file."
 
-# Instructions: Use this script to create a bootable iso for Fedora, RHEL
-#    CentOS, Almalinux or Rocky installation, with a kickstarter configuration
-#    file.
+# Instructions: Use this script to create a bootable ISO for Fedora, RHEL
+#    CentOS, Almalinux, Rocky, or Debian installation, with a kickstarter or
+#    preseed configuration file.
 
 # Requirements: mkisofs (cdrtools), isomd5sum (only available on Linux),
 #               syslinux (only available on Linux).
@@ -19,9 +21,11 @@
 usage() {
   echo "Usage:    $(basename "$0") [OPTIONS]"
   echo "  -i netinstall.iso               set the path of the iso file."
-  echo "  -o custom.img                   set the path of the image file or a output"
-  echo "                                  directory to create the image file."
+  echo "  -o custom.iso                   set the path of the iso file or a output"
+  echo "                                  directory to create the iso file."
   echo "  -k ks.cfg                       set the path of the kickstart configuration"
+  echo "                                  file."
+  echo "  -p preseed.cfg                  set the path of the preseed configuration"
   echo "                                  file."
 }
 
@@ -36,8 +40,7 @@ if ! which mkisofs >/dev/null 2>&1; then
   exit 1
 fi
 
-# On Linux Check if isomd5sum and syslinux are installed and request sudo access
-# for mounting the iso image.
+# On Linux Check if isomd5sum and syslinux are installed.
 if [[ "$OSTYPE" != "darwin"* ]]; then
   if ! rpm -q --quiet isomd5sum; then
     echo "This script requires isomd5sum to be installed."
@@ -52,12 +55,13 @@ fi
 echo "Your sudo password is required for mounting the ISO image."
 sudo echo
 
-OPTS="i:,o:,k:"
+OPTS="i:,o:,k:,p:"
 while getopts $OPTS OPTIONS; do
    case $OPTIONS in
-    i) ISO=$(readlink -f $OPTARG) ;;
-    o) IMG=$(touch $OPTARG; readlink -f $OPTARG) ;;
-    k) KS=$(readlink -f $OPTARG) ;;
+    i) ISO=$(readlink -f "$OPTARG") ;;
+    o) IMG=$(touch "$OPTARG"; readlink -f "$OPTARG") ;;
+    k) KS=$(readlink -f "$OPTARG") ;;
+    p) PRESEED=$(readlink -f "$OPTARG") ;;
     ?) usage; exit 1;;
   esac
 done
@@ -67,8 +71,8 @@ if [ -z "$ISO" ]; then
   echo "ISO file not found."; exit 1
 elif [ -z "$IMG" ]; then
   echo "Output file or directory not found."; exit 1
-elif [ -z "$KS" ]; then
-  echo "Kickstart file not found."; exit 1
+elif [ -z "$KS" ] && [ -z "$PRESEED" ]; then
+  echo "Kickstart or preseed file not found."; exit 1
 fi
 
 echo "+--------------------------------------------------------+"
@@ -85,38 +89,41 @@ BUILD=$WORKDIR/build
 MOUNT=$WORKDIR/mount
 
 echo "> Copying files.."
-mkdir -p $BUILD $MOUNT
+mkdir -p "$BUILD" "$MOUNT"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sudo mount -o ro,loop "$ISO" $MOUNT
-  cp -aRT $MOUNT $BUILD
+  sudo mount -o ro,loop "$ISO" "$MOUNT"
+  cp -aRT "$MOUNT" "$BUILD"
+  sudo chown -R "$USER" "$BUILD"
+  chmod -R +w "$BUILD"
   sudo umount "$ISO"
-  rm -rf $MOUNT
+  rm -rf "$MOUNT"
 else
   DISK=$(hdiutil attach -nobrowse -nomount "$ISO" \
     | grep -o "/dev/disk[0-9]\+" \
     | head -1)
-  mount -t cd9660 $DISK $MOUNT
-  sudo cp -R $MOUNT/ $BUILD >/dev/null
-  sudo chown -R $USER $BUILD
-  chmod -R +w $BUILD
+  mount -t cd9660 "$DISK" "$MOUNT"
+  sudo cp -R "$MOUNT/" "$BUILD" >/dev/null
+  sudo chown -R "$USER" "$BUILD"
+  chmod -R +w "$BUILD"
   umount "$DISK"
-  rm -rf $MOUNT
+  rm -rf "$MOUNT"
 fi
 
-
 if [ -n "$KS" ]; then
+  EFI_IMAGE="images/efiboot.img"
+
   echo "> Adding kickstart configuration.."
-  cp "$KS" $BUILD/ks.cfg
+  cp "$KS" "$BUILD/ks.cfg"
 
   # Legacy boot
-  if [ -f $BUILD/isolinux/isolinux.cfg ]; then
+  if [ -f "$BUILD/isolinux/isolinux.cfg" ]; then
     LABEL=$(sed -n 's/menu label \^Install \(.*\)/\1/p' \
-      $BUILD/isolinux/isolinux.cfg \
+      "$BUILD/isolinux/isolinux.cfg" \
       | sed -e 's/^ *//' -e 's/ *$//')
-    ID=$(grep hd:LABEL= $BUILD/isolinux/isolinux.cfg \
+    ID=$(grep hd:LABEL= "$BUILD/isolinux/isolinux.cfg" \
       | head -1 \
       | sed 's/.*=hd:LABEL=\(.*\)\s.*/\1/')
-    sed -i '/menu default/d' $BUILD/isolinux/isolinux.cfg
+    sed -i '/menu default/d' "$BUILD/isolinux/isolinux.cfg"
     sed -i "/^label check$/{N; /^label check/ i \\
 label linux_ks \\
   menu default \\
@@ -124,39 +131,94 @@ label linux_ks \\
   kernel vmlinuz \\
   append initrd=initrd.img inst.stage2=hd:LABEL=$ID inst.ks=hd:LABEL=$ID:/ks.cfg inst.sshd quiet \\
 
-}" $BUILD/isolinux/isolinux.cfg
+}" "$BUILD/isolinux/isolinux.cfg"
   fi
 
   # UEFI boot
-  if [ -f $BUILD/EFI/BOOT/grub.cfg ]; then
+  if [ -f "$BUILD/EFI/BOOT/grub.cfg" ]; then
     LABEL=$(sed -n 's/^menuentry \x27Install \(.*\)\x27.*/\1/p' \
-      $BUILD/EFI/BOOT/grub.cfg)
-    ID=$(grep hd:LABEL= $BUILD/EFI/BOOT/grub.cfg \
+      "$BUILD/EFI/BOOT/grub.cfg")
+    ID=$(grep hd:LABEL= "$BUILD/EFI/BOOT/grub.cfg" \
       | head -1 \
       | sed 's/.*=hd:LABEL=\(.*\)\s.*/\1/')
-    # sed -i 's/^set default="1"/set default="2"/' $BUILD/EFI/BOOT/grub.cfg
-    sed -iv "/^menuentry \x27Test/{N; /^menuentry \x27Test/ i \\
+    # sed -i 's/^set default="1"/set default="2"/' "$BUILD/EFI/BOOT/grub.cfg"
+    sed -i "/^menuentry \x27Test/{N; /^menuentry \x27Test/ i \\
 menuentry 'Install $LABEL with Kickstart' --class fedora --class gnu-linux --class gnu --class os { \\
         linuxefi /images/pxeboot/vmlinuz inst.stage2=hd:LABEL=$ID inst.ks=hd:LABEL=$ID:/ks.cfg inst.sshd quiet \\
         initrdefi /images/pxeboot/initrd.img \\
 }
 
-}" $BUILD/EFI/BOOT/grub.cfg
-    [ -f $BUILD/EFI/BOOT/BOOT.conf ] && \
-      cp $BUILD/EFI/BOOT/grub.cfg $BUILD/EFI/BOOT/BOOT.conf
+}" "$BUILD/EFI/BOOT/grub.cfg"
+    [ -f "$BUILD/EFI/BOOT/BOOT.conf" ] && \
+      cp "$BUILD/EFI/BOOT/grub.cfg" "$BUILD/EFI/BOOT/BOOT.conf"
   fi
 
 fi
 
+if [ -n "$PRESEED" ]; then
+  EFI_IMAGE="boot/grub/efi.img"
+  ID="$(isoinfo -d -i "$ISO" | grep Volume\ id: | sed -e 's/Volume id:\(.*\)/\1/' | xargs)"
+
+  echo "> Adding preseed configuration.."
+  cp "$PRESEED" "$BUILD/preseed.cfg"
+
+  # Legacy boot
+  if [ -f "$BUILD/isolinux/isolinux.cfg" ]; then
+    # add preseed.cfg to all auto install options
+    for file in $(grep -Rl auto=true  "$BUILD/isolinux"); do
+      sed -i 's/auto=true priority=critical/file=\/cdrom\/preseed.cfg auto=true priority=critical locale=en_US/' "$file"
+    done
+    # remove default
+    sed -i -e 's/^default/# default/' -e 's/menu default/# menu default/' "$BUILD/isolinux/gtk.cfg"
+    # remove autoboot for speech-enabled install
+    sed -i -e 's/^timeout/# timeout/' \
+      -e 's/^ontimeout/# ontimeout/' \
+      -e 's/^menu autoboot/# menu autoboot/' \
+      "$BUILD/isolinux/spkgtk.cfg"
+    # add new entry to default menu
+    cat >> "$BUILD/isolinux/txt.cfg" <<EOF
+default auto
+label auto
+	menu label ^Automated install
+  menu default
+	kernel /install.amd/vmlinuz
+	append file=/cdrom/preseed.cfg auto=true priority=critical locale=en_US vga=788 initrd=/install.amd/initrd.gz --- quiet
+# timeout to automated install install
+timeout 300
+ontimeout /install.amd/vmlinuz file=/cdrom/preseed.cfg auto=true priority=critical locale=en_US vga=788 initrd=/install.amd/initrd.gz --- quiet
+menu autoboot Press a key, otherwise Automated install will be started in # second{,s}...
+EOF
+  fi
+
+  # UEFI boot
+  if [ -f "$BUILD/boot/grub/grub.cfg" ]; then
+    sed -i 's/auto=true priority=critical/file=\/cdrom\/preseed.cfg auto=true priority=critical locale=en_US/g' "$BUILD/boot/grub/grub.cfg"
+    echo -e "set default=2\nset timeout=30\n$(cat "$BUILD/boot/grub/grub.cfg")" > "$BUILD/boot/grub/grub.cfg"
+    sed -i "/^submenu --hotkey=a 'Advanced options ...'/ i \\
+menuentry --hotkey=a 'Automated install' { \\
+    set background_color=black \\
+    linux    /install.amd/vmlinuz file=/cdrom/preseed.cfg auto=true priority=critical locale=en_US vga=788 --- quiet \\
+    initrd   /install.amd/initrd.gz \\
+}
+" "$BUILD/boot/grub/grub.cfg"
+  fi
+
+cd "$BUILD" || exit
+
+# Update md5sum
+md5sum "$(find -L . -type f ! -name md5sum.txt ! -name boot.cat ! -name isolinux.bin)" > md5sum.txt
+
+fi
+
 echo "> Creating image file.."
-cd $BUILD || exit
-mkisofs -quiet -untranslated-filenames -volid "$ID" \
+cd "$BUILD" || exit
+mkisofs -quiet -untranslated-filenames -V "$ID" \
   -J -joliet-long -rational-rock -translation-table -input-charset utf-8 \
   -x ./lost+found -b isolinux/isolinux.bin -c isolinux/boot.cat \
   -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot \
-  -b images/efiboot.img -no-emul-boot \
+  -b "$EFI_IMAGE" -no-emul-boot \
   -o "$IMG" \
-  -T $BUILD &>/dev/null
+  -T "$BUILD" &>/dev/null
 
 if [[ "$OSTYPE" != "darwin"* ]]; then
   echo "> Update md5sum.."
